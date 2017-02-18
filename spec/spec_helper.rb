ed64e7e8f159f9e5848122caeecb204816c1f0cb
Epigene <augusts.bautra@gmail.com>
2017-02-18 15:19:58 +0200

    Refactored generation expectations out into helper file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,21 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+
+
+require 'simplecov'
+SimpleCov.start do
+  puts "--required simplecov"
+
+  add_filter "/bin/"
+  add_filter "/spec/"
+  add_filter "rspec/scaffold/tasks/"
+  # add_group "Long files" do |src_file|
+  #   src_file.lines.count > 100
+  # end
+end
+
 require 'rspec/scaffold'
-FIXTURE_ROOT = Pathname.new(File.join File.expand_path('..', __FILE__), 'fixtures')
+FIXTURE_ROOT = Pathname.new(File.join File.expand_path('..', __FILE__), 'test_files')
 Dir[
   FIXTURE_ROOT.join('**', '*.rb')
 ].sort.each &method(:require)
@@ -11,3 +25,4 @@ RSpec.configure do |config|
 end
 
 require 'pry'
+require 'spec_setup_methods'
